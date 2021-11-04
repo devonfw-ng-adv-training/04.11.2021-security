b30df02757e6854822329c9063748b9886d8d598
Marek Matczak <marek.matczak79@gmail.com>
2021-11-04 19:12:37 +0100

    Authorization service added

--- src/app/security/model/index.ts
+++ src/app/security/model/index.ts
@@ -2,3 +2,5 @@ export interface SecurityContext {
   jwtAccessToken: string;
   email: string;
 }
+
+export type UserRight = 'newBook' | 'updateBook';
--- src/app/security/services/security.service.ts
+++ src/app/security/services/security.service.ts
@@ -2,14 +2,26 @@ import {Auth, CognitoHostedUIIdentityProvider} from '@aws-amplify/auth';
 import {Hub} from '@aws-amplify/core';
 import {Inject, Injectable, InjectionToken} from '@angular/core';
 import {Router} from '@angular/router';
-import {SecurityContext} from '../model';
+import {SecurityContext, UserRight} from '../model';
+import {Observable, of} from 'rxjs';
+import {map, shareReplay, switchMap} from 'rxjs/operators';
+import {fromPromise} from 'rxjs/internal-compatibility';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 export const AMPLIFY_CONFIG = new InjectionToken('AMPLIFY_CONFIG');
 
+interface User {
+  email: string;
+  rights: UserRight[];
+}
+
 @Injectable()
 export class SecurityService {
+  private currentUserRights$: Observable<UserRight[]> | undefined;
+
   constructor(@Inject(AMPLIFY_CONFIG) amplifyConfig: any,
-              private readonly router: Router) {
+              private readonly router: Router,
+              private readonly http: HttpClient) {
     Auth.configure(amplifyConfig);
     Hub.listen('auth', ({payload: {event, data}}) => {
       if (event === 'customOAuthState') {
@@ -37,5 +49,30 @@ export class SecurityService {
 
   signOut(): Promise<void> {
     return Auth.signOut();
+  }
+
+  userHasRight(right: UserRight): Observable<boolean> {
+    return this.getCurrentUserRightsPossiblyCached()
+      .pipe(map(currentUserRights => currentUserRights.includes(right)))
+  }
+
+  private getCurrentUserRightsPossiblyCached(): Observable<UserRight[]> {
+    if (!this.currentUserRights$) {
+      this.currentUserRights$ = fromPromise(this.getContext())
+        .pipe(
+          map(context => context?.email),
+          switchMap(email => email ? this.getUserRightsBy(email) : of([])),
+          shareReplay(1)
+        )
+    }
+    return this.currentUserRights$;
+  }
+
+  private getUserRightsBy(userEmail: string): Observable<UserRight[]> {
+    const params = new HttpParams().set('email', userEmail);
+    return this.http.get<User[]>('api/users', {params})
+      .pipe(
+        map(users => users?.[0].rights ?? [])
+      )
   }
 }
