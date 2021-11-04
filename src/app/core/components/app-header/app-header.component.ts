9faeca7277fa2780a67de69b3eb78e35c7eba202
Marek Matczak <marek.matczak79@gmail.com>
2021-11-04 18:20:42 +0100

    Sign In, Sign Up and Sign Out buttons added

--- src/app/core/components/app-header/app-header.component.ts
+++ src/app/core/components/app-header/app-header.component.ts
@@ -1,9 +1,28 @@
-import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {SecurityService} from '../../../security/services/security.service';
 
 @Component({
   selector: 'ba-app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss']
 })
-export class AppHeaderComponent {
+export class AppHeaderComponent implements OnInit{
+  userEmail: string | null = null;
+
+  constructor(private readonly security: SecurityService,
+              private readonly router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.security.getContext().then(context => this.userEmail = context?.email ?? null)
+  }
+
+  goToSignInDialog(): Promise<void> {
+    return this.security.goToSignInDialog();
+  }
+
+  signOut(): Promise<boolean> {
+    return this.security.signOut().then(() => this.router.navigateByUrl('/'));
+  }
 }
