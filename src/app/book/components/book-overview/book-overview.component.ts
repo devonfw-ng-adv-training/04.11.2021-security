6c34f87c82d3405624e9128c6a1f06287ed4c50b
Marek Matczak <marek.matczak79@gmail.com>
2021-11-04 22:10:31 +0100

    Going to details from overview only for authorized users

--- src/app/book/components/book-overview/book-overview.component.ts
+++ src/app/book/components/book-overview/book-overview.component.ts
@@ -1,25 +1,41 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {Book} from '../../model/book';
 import {BookService} from '../../services/book.service';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import {ActivatedRoute, Router} from '@angular/router';
-import {delay} from 'rxjs/operators';
+import {SecurityService} from '../../../security/services/security.service';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'ba-book-overview',
   templateUrl: './book-overview.component.html',
   styleUrls: ['./book-overview.component.scss']
 })
-export class BookOverviewComponent {
+export class BookOverviewComponent implements OnDestroy {
   books$: Observable<Book[]>;
+  userHasRightToUpdateBook$: Observable<boolean>;
+  private readonly unsubscribe = new Subject();
 
   constructor(private books: BookService,
               private router: Router,
-              private currentRoute: ActivatedRoute) {
+              private currentRoute: ActivatedRoute,
+              private readonly security: SecurityService) {
     this.books$ = books.findAll();
+    this.userHasRightToUpdateBook$ = this.security.userHasRight('updateBook');
   }
 
   goToDetailsOf(book: Book): void {
-    this.router.navigate([book.id], {relativeTo: this.currentRoute});
+    this.userHasRightToUpdateBook$
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(isAuthorized => {
+        if (isAuthorized) {
+          this.router.navigate([book.id], {relativeTo: this.currentRoute});
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 }
